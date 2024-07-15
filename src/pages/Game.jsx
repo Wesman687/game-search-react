5d8088f392d3e0406b011629b64705e9ef963972
Paul <wesman687@gmail.com>
2024-07-15 16:42:41 -0400

    update

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -7,52 +7,37 @@ import axios from "axios";
 const Game = () => {
   const [games, setGames] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [sortedGames, setSort] = useState([])
-  const [search, setSearch] = useState("")
-  const [tagArray, setTag] = useState([])
+  const [sortedGames, setSort] = useState([]);
+  const [search, setSearch] = useState("");
+  const [tagArray, setTag] = useState([]);
   async function searchBar() {
-    const tags = "mmorpg, shooter, strategy, moba, racing, sports, social, sandbox, open-world, survival, pvp, pve, pixel, voxel, zombie, turn-based, first-person, third-Person, top-down, tank, space, sailing, side-scroller, superhero, permadeath, card, battle-royale, mmo, mmofps, mmotps, 3d, 2d, anime, fantasy, sci-fi, fighting, action-rpg, action, military, martial-arts, flight, low-spec, tower-defense, horror, mmorts"
+    const tags =
+      "mmorpg, shooter, strategy, moba, racing, sports, social, sandbox, open-world, survival, pvp, pve, pixel, voxel, zombie, turn-based, first-person, third-Person, top-down, tank, space, sailing, side-scroller, superhero, permadeath, card, battle-royale, mmo, mmofps, mmotps, 3d, 2d, anime, fantasy, sci-fi, fighting, action-rpg, action, military, martial-arts, flight, low-spec, tower-defense, horror, mmorts";
     if (search === "") {
-        return
+      return;
     }
-    if (isNaN(search) && (tags.toUpperCase().search(search.toUpperCase()) !== -1)){  
-        fetchTag(search)
-        setSort(tagArray.slice(0,6))   
+    if ((tags.toUpperCase().search(search.toUpperCase())) && search.toUpperCase() !== "RPG"){
+      fetchTag(search);
+      setSort(tagArray.slice(0, 6));
+    } else if (search.length === 4 && !isNaN(search)) {
+      setSort(games.filter((game) => search === game.release_date.slice(0, 4)));
     }
     else {
-        if (search.length === 4 && !(isNaN(search))) {
-            setSort(games.filter(game => search === game.release_date.slice(0,4)))
+        let tempArray = games.filter(game =>  game.title.toUpperCase().match(search.toUpperCase()))
+        let secondTemp = games.filter(game =>((game.short_description.toUpperCase().match(search.toUpperCase()) && (game.title !== tempArray[0].title))))
+        for (let i=0; i < 6; i++){
+            tempArray.push(secondTemp[i])
         }
+        setSort(tempArray.slice(0,6))
     }
-
   }
   async function fetchTag(tag) {
     const options = {
-        method: 'GET',
-        url: 'https://mmo-games.p.rapidapi.com/games',
-        params: {
-          category: `${tag}`
-        },
-        headers: {
-          'x-rapidapi-key': '477cc3938dmsh3d6acb4fdba02afp1d7380jsn03a9325cb48a',
-          'x-rapidapi-host': 'mmo-games.p.rapidapi.com'
-        }
-      };
-      
-      try {
-          const response = await axios.request(options);
-          setTag(response.data)
-      } catch (error) {
-          console.error(error);
-      }
-
-  }
-  async function fetchGames() {
-    setLoading(true)
-    const options = {
       method: "GET",
       url: "https://mmo-games.p.rapidapi.com/games",
-      platform: 'browser',
+      params: {
+        category: `${tag}`,
+      },
       headers: {
         "x-rapidapi-key": "477cc3938dmsh3d6acb4fdba02afp1d7380jsn03a9325cb48a",
         "x-rapidapi-host": "mmo-games.p.rapidapi.com",
@@ -61,17 +46,36 @@ const Game = () => {
 
     try {
       const response = await axios.request(options);
-      setGames(response.data)
+      setTag(response.data);
     } catch (error) {
       console.error(error);
     }
-    setSort(games.slice(0,6)) 
-    setLoading(false)
   }
-  
-    useEffect(()=>{
-        fetchGames()
-    },[])
+  async function fetchGames() {
+    setLoading(true);
+    const options = {
+      method: "GET",
+      url: "https://mmo-games.p.rapidapi.com/games",
+      platform: "browser",
+      headers: {
+        "x-rapidapi-key": "477cc3938dmsh3d6acb4fdba02afp1d7380jsn03a9325cb48a",
+        "x-rapidapi-host": "mmo-games.p.rapidapi.com",
+      },
+    };
+
+    try {
+      const response = await axios.request(options);
+      setGames(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+    setSort(games.slice(0, 6));
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchGames();
+  }, []);
   return (
     <div className="row">
       <Nav />
@@ -84,37 +88,48 @@ const Game = () => {
         </h2>
         <div className="input__wrapper">
           <input
-            onKeyDown={(event)=> event.key === 'Enter' && searchBar()}
-            onChange={(event)=>setSearch(event.target.value)}
+            onKeyDown={(event) => event.key === "Enter" && searchBar()}
+            onChange={(event) => setSearch(event.target.value)}
             type="text"
             id="input"
             placeholder="Search for Game here"
             className="input__bar"
           />
-          <button onClick={()=>searchBar()} className="submit__btn">
+          <button onClick={() => searchBar()} className="submit__btn">
             Submit
           </button>
         </div>
         <div className="popular__games games">
-        {loading?<FontAwesomeIcon icon="fas fa-spinner" className="fa-spinner--games" />:sortedGames.map((games) => 
-    (
-        
-    <div className="popular__game click">
-    <h3 className="game__link--title shadoww">{games.title}</h3>
-        <div className="popular__img--wrapper">
-        <figure className="popular__imgs">
-            <img src={games.thumbnail} className="popular__img" alt=""></img>
-        </figure>
-        </div>
-        <div className="popular__text--wrapper">
-        <a href="${games.game_url}" className="game__link--text">{games.game_url}</a>
-        <p className="game__genre--text ">{games.genre}</p>
-        <p className="game__developer--text shadoww">{games.publisher}</p>
-        </div>
-    </div>
-    
-    ))}
-        
+          {loading ? (
+            <FontAwesomeIcon
+              icon="fas fa-spinner"
+              className="fa-spinner--games"
+            />
+          ) : (
+            sortedGames.map((games) => (
+              <div className="popular__game click">
+                <h3 className="game__link--title shadoww">{games.title}</h3>
+                <div className="popular__img--wrapper">
+                  <figure className="popular__imgs">
+                    <img
+                      src={games.thumbnail}
+                      className="popular__img"
+                      alt=""
+                    ></img>
+                  </figure>
+                </div>
+                <div className="popular__text--wrapper">
+                  <a href="${games.game_url}" className="game__link--text">
+                    {games.game_url}
+                  </a>
+                  <p className="game__genre--text ">{games.genre}</p>
+                  <p className="game__developer--text shadoww">
+                    {games.publisher}
+                  </p>
+                </div>
+              </div>
+            ))
+          )}
         </div>
       </div>
       <Footer />
